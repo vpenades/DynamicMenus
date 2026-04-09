ad672c3ffaa8d28fe7a4c0598754da463e77180e
Vicente Penades Armengot <5433822+vpenades@users.noreply.github.com>
2026-04-09 12:06:27 +0200

    upgraded to Avalonia 12

--- examples/DynamicMenusDemo.AvaloniaApp/App.axaml.cs
+++ examples/DynamicMenusDemo.AvaloniaApp/App.axaml.cs
@@ -19,10 +19,7 @@ namespace DynamicMenusDemo.AvaloniaApp
         public override void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                // Line below is needed to remove Avalonia data validation.
-                // Without this line you will get duplicate validations from both Avalonia and CT
-                BindingPlugins.DataValidators.RemoveAt(0);
+            {                
                 desktop.MainWindow = new MainWindow
                 {
                     DataContext = new MainWindowViewModel(),
